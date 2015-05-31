c36fc4d2cec8b469149d4046cf69073f3683a4da
Stefanie Taepke <s_t@me.com>
2015-05-31 08:17:24 +0200

    rackt/routing throws error
    Uncaught Error: Invariant Violation: _registerComponent(...): Target container is not a DOM element.

--- src/app/components/PokemonApp.react.js
+++ src/app/components/PokemonApp.react.js
@@ -1,20 +1,38 @@
-var Header = require('./Header.react'),
-    Footer = require('./Footer.react'),
-    MainSection = require('./MainSection.react');
-
 var React = require('react'),
-    PokemonApp = React.createClass({
+    Header = require('./Header.react'),
+    Footer = require('./Footer.react'),
+    MapSection = require('./MapSection.react'),
+    MainSection = require('./MainSection.react');
+var Router = require('react-router');
+var Route = Router.Route,
+    DefaultRoute = Router.DefaultRoute,
+    RouteHandler = Router.RouteHandler,
+    Link = Router.Link;
+
+var PokemonApp = React.createClass({
     render: function() {
         return (
             /*jshint ignore:start */
             <div>
                 <Header />
-                <MainSection />
+                <RouteHandler />
                 <Footer />
             </div>
             /*jshint ignore:end */
         );
     }
+});
+
+var routes = (
+    <Route name="app" path="/" handler={PokemonApp}>
+        <Route name="map" path="/map" handler={MapSection} />
+        <Route name="stuff" path="/stuff" handler={MainSection} />
+        <DefaultRoute handler={MapSection}/>
+    </Route>
+);
+
+Router.run(routes, function (Handler) {
+    React.render(<Handler />, document.body);
 });
 
 module.exports = PokemonApp;
